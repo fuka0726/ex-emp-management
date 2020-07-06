847aed2440d418f9a2a35a57a76904b0e55ba3f5
fuka0726 <fuuka.pyop@gmail.com>
2020-07-06 18:13:34 +0900

    EmployeeRepository作成

---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,73 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+@Repository
+public class EmployeeRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+		Employee employee = new Employee();
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getDate("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipcode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address"));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("dependentsCount"));
+		return employee;
+		
+	};
+	
+	/**
+	 * 従業員一覧情報を入社日順で取得.
+	 * @return 従業員一覧
+	 */
+	public List<Employee> findAll(){
+		String sql = "select id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependentsCount "
+				+ " from employees order by hire_date DESC ";
+		List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+		return employeeList;
+	}
+	
+	/**
+	 * 主キーから従業員情報を取得する.
+	 * @param id 検索したい従業員ID
+	 * @return　検索された従業員情報
+	 */
+	public Employee load(Integer id) {
+		String sql = "select id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependentsCount "
+				+ " from employee where id = :id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+		return employee;
+	}
+	
+	/**
+	 * 従業員情報を変更する.
+	 * @param employee
+	 */
+	public void update(Employee employee) {
+		String sql = "update employees set dependentsCount = :dependentsCount where id = :id ";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		template.update(sql, param);
+	}
+	
+}
